4eaac5ece6b8f75551edab7d9654de941a00a8ac
Rachez <jiazhuochen.web@gmail.com>
2016-04-23 10:51:26 +0800

    qrcode

--- app/Http/Controllers/Wechat/QrcodeController.php
+++ app/Http/Controllers/Wechat/QrcodeController.php
@@ -19,6 +19,11 @@ class QrcodeController extends Controller
         $result = $qrcode->temporary('test', 7*24*3600);
         $ticket = $result->ticket;
         $url = $result->url;
+        $url = $qrcode->url($ticket);
+        $content = file_get_contents($url);
+
+        $image = new Image();
+        $image->storeImage('QrCode', $content, 'jpg', false);
         echo "<img src={$url}>";
     }
 }
