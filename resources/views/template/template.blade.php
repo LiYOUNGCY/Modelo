097d21875838c00493320c5a93fbd1ad194ecf40
Rachez <jiazhuochen.web@gmail.com>
2016-05-06 16:56:26 +0800

    'fix'

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -2,9 +2,9 @@
 
 @section('css')
     <link rel="stylesheet" href="{{ asset('assets') }}/css/bootstrap.min.css?v={{time()}}">
-    <link rel="stylesheet" href="{{ asset('assets') }}/css/base.css?v={{time()}}">
     <link rel="stylesheet" href="{{ asset('assets') }}/font-awesome/css/font-awesome.min.css?">
     @yield('moreCss')
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/base.css?v={{time()}}">
 @endsection
 
 @section('script')
