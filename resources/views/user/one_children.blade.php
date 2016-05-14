37ad0a5a01d2cfd27bdfafd0463f3d34f8f36341
Rachez <jiazhuochen.web@gmail.com>
2016-05-14 13:06:05 +0800

    'fix'

--- resources/views/user/one_children.blade.php
+++ resources/views/user/one_children.blade.php
@@ -14,20 +14,64 @@
         </div>
         <div class="m-son">
             <div class="search-son">
-                <input type="text" name="sonName" placeholder="搜索">
+                <input type="text" id="name" name="name" placeholder="搜索">
             </div>
-            <div class="son-list">
+            <div class="son-list" id="list">
                 @foreach($children as $item)
-                <div class="son-item">
-                    <span >{{ $item->nickname }}</span>
-                    @if($item->flag)
-                        <span class="son-level active">大豆芽</span>
-                    @else
-                        <span class="son-level">大豆芽</span>
-                    @endif
-                </div>
+                    <div class="son-item">
+                        <span data-type="name">{{ $item->nickname }}</span>
+                        @if($item->flag)
+                            <span class="son-level active">大豆芽</span>
+                        @else
+                            <span class="son-level">大豆芽</span>
+                        @endif
+                    </div>
                 @endforeach
+                    <div class="son-item">
+                        <span data-type="name">弱鸡一号</span>
+                        <span class="son-level ">大豆芽</span>
+                    </div>
+                    <div class="son-item">
+                        <span data-type="name">弱鸡一号</span>
+                        <span class="son-level ">大豆芽</span>
+                    </div>
+                    <div class="son-item">
+                        <span data-type="name">弱鸡二号b</span>
+                        <span class="son-level ">大豆芽</span>
+                    </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('moreScript')
+    <script>
+        $(function () {
+            $('#name').bind('input propertychange', function () {
+                var keyword = $(this).val();
+                console.log(keyword);
+
+                $('#list').find('.son-item').each(function (i, ele) {
+                    var name = $(ele).find('span[data-type=name]')[0];
+                    name = $(name).html();
+//                    console.log(name);
+                    console.log(name.indexOf(keyword));
+
+                    if ( name.indexOf(keyword) >= 0) {
+                        $(ele).attr('data-disable', '0');
+                    } else {
+                        $(ele).attr('data-disable', '1');
+                    }
+                });
+
+                $('#list').find('.son-item').each(function (i, ele) {
+                    if($(ele).attr('data-disable') == 1) {
+                        $(ele).hide();
+                    } else {
+                        $(ele).show();
+                    }
+                });
+            });
+        });
+    </script>
 @endsection
