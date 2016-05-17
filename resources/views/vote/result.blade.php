5ce63d9f23b61b97eb5f430788372fbcddfca5aa
Rachez <jiazhuochen.web@gmail.com>
2016-05-18 01:39:29 +0800

    'fix'

--- resources/views/vote/result.blade.php
+++ resources/views/vote/result.blade.php
@@ -29,6 +29,7 @@
         <p>1.如果你无法简洁的表达你的想法，那只说明你还不够了解它。</p>
         <p>1.如果你无法简洁的表达你的想法，那只说明你还不够了解它。</p>
     </div>
+    <div class="result-title">投票结果</div>
     <div class="vote-list">
         <div class="vote-item active">
             <div class="vote-info">
