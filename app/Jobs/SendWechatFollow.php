ce0d87b6b12bdee665a455f7943704191b267ac4
Rachez <jiazhuochen.web@gmail.com>
2016-05-29 19:35:58 +0800

    'fix'

--- app/Jobs/SendWechatFollow.php
+++ app/Jobs/SendWechatFollow.php
@@ -29,7 +29,7 @@ class SendWechatFollow extends Job implements ShouldQueue
         $url= url('/');
         $message = new Text(['content' => "谢谢那么好看的你来加入魔豆世界，这里是有颜走心的购物分享平台。了解我们请点击<a href='http://mp.weixin.qq.com/s?__biz=MzIyMjIwMjA4Mw==&mid=100000082&idx=1&sn=771c9e17262385afe7048b54e274dc74#rd'>这里</a>，喜欢我们的原创设计点击<a href='{$url}'>这里</a>购买。"]);
         $app = app('wechat');
-
-        $result = $app->staff->message($message)->to($this->openId)->send();
+        $broadcast = $app->broadcast;
+        $broadcast->sendText($message, [$this->openId]);
     }
 }
