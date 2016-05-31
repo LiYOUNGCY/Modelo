eba8ae7f7cbd52a60d5f5112e70e8ae48240f134
Rachez <jiazhuochen.web@gmail.com>
2016-05-31 14:12:20 +0800

    'fix'

--- app/Http/Controllers/Wechat/MenuController.php
+++ app/Http/Controllers/Wechat/MenuController.php
@@ -21,16 +21,16 @@ class MenuController extends Controller
             ],
             [
                 'type' => 'view',
-                'name' => '参与投票',
-                'url' => url('vote'),
+                'name' => '优惠活动',
+                'url' => 'http://mp.weixin.qq.com/s?__biz=MzIyMjIwMjA4Mw==&mid=100000070&idx=1&sn=2949a971cdd2244dc489dd88a3745737#rd',
             ],
             [
                 'name' => '我的助手',
                 'sub_button' => [
                     [
                         'type' => 'view',
-                        'name' => '优惠活动',
-                        'url' => 'http://mp.weixin.qq.com/s?__biz=MzIyMjIwMjA4Mw==&mid=100000070&idx=1&sn=2949a971cdd2244dc489dd88a3745737#rd',
+                        'name' => '参与投票',
+                        'url' => url('vote'),
                     ],
                     [
                         'type' => 'view',
@@ -51,5 +51,7 @@ class MenuController extends Controller
             ],
         ];
         $menu->add($buttons);
+
+        echo 'Update Success';
     }
 }
